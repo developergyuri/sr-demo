050ed07b9f2fd708864304a9f7fafad5e60f02fa
Rogoz György <developergyuri@gmail.com>
2021-11-03 17:37:05 +0100

    added mobile menu to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 
 /* Styles */
 import styles from "./Header.module.css";
@@ -8,54 +9,81 @@ import { ReactComponent as ArrowDownIcon } from "../../assets/header_arrow.svg";
 import { ReactComponent as ProfileIcon } from "../../assets/header_profile.svg";
 import { ReactComponent as HeartIcon } from "../../assets/header_heart.svg";
 import { ReactComponent as BagIcon } from "../../assets/header_bag.svg";
+import { ReactComponent as VialIcon } from "../../assets/subheader_vial.svg";
 import Navigation from "../ui/navigation/Navigation";
 import Searchbar from "../ui/searchbar/Searchbar";
 import Logo from "../ui/logo/Logo";
 
 const Header = () => {
+  const [drawer, setDrawer] = useState(false);
+
   const iconClickHandler = (value) => {
     alert(`Clicked: ${value}`);
   };
 
+  const drawerHandler = () => {
+    setDrawer(!drawer);
+  };
+
+  const drawerIconClasses = classNames(styles.drawerIcon, {
+    [`${styles.isClicked}`]: drawer,
+  });
+
+  const menuClasses = classNames(styles.menu, {
+    [`${styles.isOpened}`]: drawer,
+  });
+
   return (
-    <header className={styles.container}>
-      <div className={styles.infos}>
-        <span className={styles.infoTel}>09 87 654 3210</span>
-        <div className={styles.infoLinks}>
-          <a className={styles.link} href="/">
-            Kapcsolat
-          </a>
-          <a className={styles.link} href="/">
-            Rólunk
-          </a>
-          <a className={styles.link} href="/">
-            Fizetés, szállítás
-            <ArrowDownIcon className={styles.linkIcon} />
-          </a>
+    <div className={styles.headerWrapper}>
+      <header className={styles.header}>
+        <div className={styles.infos}>
+          <span className={styles.infoTel}>09 87 654 3210</span>
+          <div className={styles.infoLinks}>
+            <a className={styles.link} href="/">
+              Kapcsolat
+            </a>
+            <a className={styles.link} href="/">
+              Rólunk
+            </a>
+            <a className={styles.link} href="/">
+              Fizetés, szállítás
+              <ArrowDownIcon className={styles.linkIcon} />
+            </a>
+          </div>
         </div>
+        <div className={styles.logo}>
+          <Logo link="/" simple="Teszt" highlighted="téma" />
+        </div>
+        <div className={styles.actions}>
+          <Searchbar />
+          <div className={styles.actionIcons}>
+            <ProfileIcon
+              className={styles.actionIcon}
+              onClick={() => iconClickHandler("Profile")}
+            />
+            <HeartIcon
+              className={styles.actionIcon}
+              onClick={() => iconClickHandler("Heart")}
+            />
+            <BagIcon
+              className={styles.actionIcon}
+              onClick={() => iconClickHandler("Bag")}
+            />
+          </div>
+        </div>
+        <div className={drawerIconClasses} onClick={drawerHandler} />
+        <nav className={menuClasses}>
+          <Navigation />
+        </nav>
+      </header>
+      <div className={styles.subHeader}>
+        <VialIcon />
+        <span>kizárólag növényi eredetű alkotó elemek</span>
       </div>
-      <div className={styles.logo}>
-        <Logo link="/" simple="Teszt" highlighted="téma" />
-      </div>
-      <div className={styles.actions}>
-        <Searchbar />
-        <ProfileIcon
-          className={styles.actionIcon}
-          onClick={() => iconClickHandler("Profile")}
-        />
-        <HeartIcon
-          className={styles.actionIcon}
-          onClick={() => iconClickHandler("Heart")}
-        />
-        <BagIcon
-          className={styles.actionIcon}
-          onClick={() => iconClickHandler("Bag")}
-        />
-      </div>
-      <nav className={styles.menu}>
-        <Navigation />
-      </nav>
-    </header>
+      {drawer && (
+        <div className={styles.drawerOverlay} onClick={drawerHandler} />
+      )}
+    </div>
   );
 };
 
